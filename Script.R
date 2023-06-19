fef3efa4ab9c0f73aac7c710488427f070be12c9
Maria Tri <mariatri13@gmail.com>
2023-06-19 23:32:50 +0300

    new data from csv

--- Script.R
+++ Script.R
@@ -1,15 +1,27 @@
 library(tidyverse)
+
 health <- read_delim("health.csv", delim = ";")
 ages <- read_delim("Ages.csv", delim = ";")
+greece <- read_delim("Greece.csv", delim = ";")
 
 View(health)
 View(ages)
+View(greece)
 
 summary(health)
 summary(ages)
 
 ages[is.na(ages)] <- 0
 health[is.na(health)] <- 0
-summary(ages)
 
-rlang::last_trace()
+summary(ages)
+summary(health)
+
+
+#Male people from 2016 in Europe countries
+ggplot(data = greece) + 
+  geom_point(mapping = aes(x = country, y = male), color = "blue")
+
+#refugee per country
+ggplot(data = greece) + 
+  geom_bar(mapping = aes(x = refugee), fill = "lightblue")
